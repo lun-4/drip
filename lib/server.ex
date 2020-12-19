9b247054868bfcfc9161a7f102a604813c67059f
Luna <git@l4.pm>
2020-12-19 16:18:56 -0300

    fix packet send

--- lib/server.ex
+++ lib/server.ex
@@ -23,6 +23,36 @@ defmodule Drip.Server do
     Packet.Ping.opcode() => Packet.Ping
   }
 
+  defp packet_atom_from_packet(packet) do
+    case packet do
+      %Packet.Unreliable.S{} -> Packet.Unreliable
+      %Packet.Reliable.S{} -> Packet.Reliable
+      %Packet.Hello.S{} -> Packet.Hello
+      %Packet.Disconnect.S{} -> Packet.Disconnect
+      %Packet.Ack.S{} -> Packet.Ack
+    end
+  end
+
+  @doc """
+  Send a packet to a given UDP socket
+  """
+  def send_packet(socket, addr, port, packet) do
+    # extract the wanted packet atom from the relevant struct, so we can
+    # get the opcode
+    packet_atom = packet_atom_from_packet(packet)
+    packet_opcode = packet_atom.opcode()
+
+    encoded =
+      packet_atom.encode(
+        "",
+        packet
+      )
+
+    # prepend opcode
+    result = <<packet_opcode>> <> encoded
+    :gen_udp.send(socket, addr, port, result)
+  end
+
   def handle_info({:udp, socket, addr, port, data}, state) do
     Logger.info("awooga #{inspect(addr)} #{inspect(port)} #{inspect(data)}")
 
@@ -36,27 +66,20 @@ defmodule Drip.Server do
       packet = packet_atom.decode(packet_rest)
       IO.inspect(packet)
 
-      encoded =
-        Packet.Disconnect.encode(
-          "",
-          %Packet.Disconnect.S{
-            reason: Map.get(Packet.Disconnect.reasons(), :forced)
-          }
-        )
+      ack = %Packet.Ack.S{
+        nonce: packet.nonce,
+        missing_packets: 255
+      }
 
-      :gen_udp.send(socket, addr, port, encoded)
+      send_packet(socket, addr, port, ack)
     else
       Logger.warn("unknown packet type #{packet_type}")
 
-      encoded =
-        Packet.Disconnect.encode(
-          "",
-          %Packet.Disconnect.S{
-            reason: Map.get(Packet.Disconnect.reasons(), :bad_connection)
-          }
-        )
+      packet = %Packet.Disconnect.S{
+        reason: Map.get(Packet.Disconnect.reasons(), :bad_connection)
+      }
 
-      :gen_udp.send(socket, addr, port, encoded)
+      send_packet(socket, addr, port, packet)
     end
 
     {:noreply, state}
