4b77ece373abd13b7d4e7791fa1abe8c9681904d
Luna <git@l4.pm>
2020-12-19 00:05:02 -0300

    add basic server logic

--- lib/server.ex
+++ lib/server.ex
@@ -2,20 +2,63 @@ defmodule Drip.Server do
   use GenServer
 
   require Logger
+  alias Drip.Protocol.Packet
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   def init(:ok) do
-    {:ok, _socket} = :gen_udp.open(5569, [:binary])
+    {:ok, _socket} = :gen_udp.open(22023, [:binary])
     Logger.info("Drip.Server: udp start")
 
     {:ok, nil}
   end
 
-  def handle_info({:udp, _socket, addr, port, data}, state) do
+  @opcode_map %{
+    Packet.Unreliable.opcode() => Packet.Unreliable,
+    Packet.Reliable.opcode() => Packet.Reliable,
+    Packet.Hello.opcode() => Packet.Hello,
+    Packet.Disconnect.opcode() => Packet.Disconnect,
+    Packet.Ping.opcode() => Packet.Ping
+  }
+
+  def handle_info({:udp, socket, addr, port, data}, state) do
     Logger.info("awooga #{inspect(addr)} #{inspect(port)} #{inspect(data)}")
+
+    <<packet_type::8, packet_rest::binary>> = data
+    IO.puts(packet_type)
+
+    packet_atom = Map.get(@opcode_map, packet_type)
+    IO.puts(packet_atom)
+
+    if packet_atom do
+      packet = packet_atom.decode(packet_rest)
+      IO.inspect(packet)
+
+      encoded =
+        Packet.Disconnect.encode(
+          "",
+          %Packet.Disconnect.S{
+            reason: Map.get(Packet.Disconnect.reasons(), :forced)
+          }
+        )
+
+      :gen_udp.send(socket, addr, port, encoded)
+    else
+      Logger.warn("unknown packet type #{packet_type}")
+
+      encoded =
+        Packet.Disconnect.encode(
+          "",
+          %Packet.Disconnect.S{
+            reason: Map.get(Packet.Disconnect.reasons(), :bad_connection)
+          }
+        )
+
+      :gen_udp.send(socket, addr, port, encoded)
+    end
+
     {:noreply, state}
   end
 end
